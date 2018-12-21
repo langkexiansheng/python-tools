a014d5499863b2794122ad6a90a0dd2183632b89
langkexiansheng <37947784+langkexiansheng@users.noreply.github.com>
2018-12-21 11:02:41 +0800

    Update udp_sniffers.py.py

--- python-网络编程/udp_sniffers.py.py
+++ python-网络编程/udp_sniffers.py.py
@@ -1,72 +1,81 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
+__author__ = 'langke'
 
-import os
 import socket
+import os
 import struct
-from ctypes import *
+from ctypes import  *
 
-HOST = '192.168.1.114'
+host="192.168.1.101"
 
-# IP 头定义
 class IP(Structure):
-	_fields_ = [
-		('ihl', c_ubyte, 4),
-		('version', c_ubyte, 4),
-		('tos', c_ubyte),
-		('len', c_ushort),
-		('id', c_ushort),
-		('offset', c_ushort),
-		('ttl', c_ubyte),
-		('protocol_num', c_ubyte),
-		('sum', c_ushort),
-		('src', c_ulong),
-		('dst', c_ulong),
-	]
+    _fields_=[
+    ("ihl",c_ubyte,4),
+    ("version",c_ubyte,4),
+    ("tos",c_ubyte),
+    ("len",c_ushort),
+    ("id",c_ushort),
+    ("offset",c_ushort),
+    ("ttl",c_ubyte),
+    ("protocol_num",c_ubyte),
+    ("sum",c_ushort),
+    ("src",c_ulong),
+    ("dst",c_ulong)
+    ]
 
-	def __new__(self, socket_buffer=None):
-		return self.from_buffer_copy(socket_buffer)
+    def  __new__(self,socket_buffer=None):
+        return self.from_buffer_copy(socket_buffer)
 
-	def __init__(self, socket_buffer=None):
-		# 协议字段与协议名称对应
-		self.protocol_map = {1:'ICMP', 6:'TCP', 17:'UDP'}
+    def __init__(self,socket_buffer=None):
+        self.protocol_map={1:"ICMP",6:"TCP",17:"UDP"}
+        self.src_address=socket.inet_ntoa(struct.pack("<L",self.src))
+        self.dst_address=socket.inet_ntoa(struct.pack("<L",self.dst))
 
-		# 可读性更强的IP地址
-		self.src_address = socket.inet_ntoa(struct.pack('<L', self.src))
-		self.dst_address = socket.inet_ntoa(struct.pack('<L', self.dst))
+        try:
+            self.protocol=self.protocol_map[self.protocol_num]
+        except:
+            self.protocol=str(self.protocol_num)
 
-		# 协议类型
-		try:
-			self.protocol = self.protocol_map[self.protocol_num]
-		except:
-			self.protocol = str(self.protocol_num)
-# 根据操作系统选择用 IP 还是 ICMP
-if os.name == 'nt':
-	socket_protocol = socket.IPPROTO_IP
+class ICMP(Structure):
+    _fields_=[
+        ("type", c_ubyte),
+        ("code",c_ubyte),
+        ("checksum",c_ushort),
+        ("unused",c_ushort),
+        ("next_hop_mtu",c_ushort)
+    ]
+
+    def __new__(self,socket_buffer):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self,socket_buffer):
+        pass
+
+
+if os.name=="nt":
+    socket_protocol=socket.IPPROTO_IP
 else:
-	socket_protocol = socket.IPPROTO_ICMP
+    socket_protocol=socket.IPPROTO_ICMP
 
-sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+sniffer=socket.socket(socket.AF_INET,socket.SOCK_RAW,socket_protocol)
+sniffer.bind((host,0))
+sniffer.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL,1)
 
-# 绑定主机，并监听 1 ： ICMP 协议
-sniffer.bind((HOST,0))
+if os.name=="nt":
+    sniffer.ioctl(socket.SIO_RCVALL,socket.RCVALL_ON)
 
-sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-
-# 打开混杂模式
-if os.name == 'nt':
-	sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 try:
-	while True:
-		raw_buffer = sniffer.recvfrom(65535)[0]
-		# 将缓冲区的前20个字节按IP头进行解析
-		ip_header = IP(raw_buffer[:20])
+    while True:
+        raw_buffer=sniffer.recvfrom(65565)[0]
+        ip_header=IP(raw_buffer[0:20])
+        print("Protocol:%s %s -> %s" %(ip_header.protocol,ip_header.src_address,ip_header.dst_address))
+        if ip_header.protocol =="ICMP":
+            offset = ip_header.ihl * 4
+            buf = raw_buffer[offset:offset + sizeof(ICMP)]
 
-		# 输出协议和通信双方IP地址
-		print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
-
-except  KeyboardInterrupt as e:
-
-	if os.name == 'nt':
-		sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-		
+            icmp_header = ICMP(buf)
+            print("ICMP -> Type:%d Code:%d " % (icmp_header.type,icmp_header.code))
+except KeyboardInterrupt:
+    if os.name=="nt":
+        sniffer.ioctl(socket.SIO_RCVALL,socket.RCVALL_OFF)	
 	print(e,'退出！')
